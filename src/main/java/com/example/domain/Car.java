03e41471fe315b1182b1fa4403f7a813978e497e
takahiroaraki <s01110002e1117.gmail.com>
2019-08-01 15:11:26 +0900

    ドメインパッケージを作成し、直下にCarクラスを作成。

---
+++ src/main/java/com/example/domain/Car.java
@@ -0,0 +1,33 @@
+package com.example.domain;
+
+public class Car {
+	private int speed;
+	private int tire;
+	private String name;
+	private int gas;
+	public int getSpeed() {
+		return speed;
+	}
+	public void setSpeed(int speed) {
+		this.speed = speed;
+	}
+	public int getTire() {
+		return tire;
+	}
+	public void setTire(int tire) {
+		this.tire = tire;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getGas() {
+		return gas;
+	}
+	public void setGas(int gas) {
+		this.gas = gas;
+	}
+
+}
